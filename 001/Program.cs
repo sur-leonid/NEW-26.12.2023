a7669b5caa464ae5cae60cd73c94f7c9d038edef
Surikov leonid <Sur-leonid@yandex.ru>
2023-01-26 22:14:58 +0300

    add 000 и 002

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,3 +1,7 @@
 ﻿// 1 С клавиатуры вводится целое число. Вывести квадрат числа (блок-схема)
-
-Console.WriteLine("Hello, World !");
+string? s;
+int a;
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+Console.WriteLine(a*a);
+Console.WriteLine(Math.Pow(a,2)); // a  в квадрате
---
+++ 002/Program.cs
@@ -0,0 +1,13 @@
+﻿// С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго (блок-схема)
+
+double a,b;
+string s;
+s=Console.ReadLine();
+a=Convert.toDouble(s);
+b=Console.ReadLine();
+a=Convert.toDouble(s);
+
+if (a*a = b)
+System.Console.WritrLine("b является квадратом а");
+if (b*b = ba)
+SystemConsole.WritrLine("a является квадратом b");
