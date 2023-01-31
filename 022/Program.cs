28ea831484c8e86b9aa0a9fd5ec7cf1c67a45314
Surikov leonid <Sur-leonid@yandex.ru>
2023-01-31 22:05:53 +0300

    add 022

---
+++ 022/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 22. По двум заданным числам проверять является ли одно квадратом другого.
+int a = 0;
+int b = 0;
+System.Console.Write("Введите a:");
+a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b:");
+b=Convert.ToInt32(Console.ReadLine());
+
+if 
+    (a * a == b)
+   { 
+    System.Console.WriteLine("b является квадратом а");
+    }
+        else
+        { 
+        System.Console.WriteLine("b НЕ является квадратом а");
+        }
+if (b * b == a)
+    {
+     System.Console.WriteLine("a является квадратом b");
+    }
+       else
+       {
+       System.Console.WriteLine("a НЕ  является квадратом  b");   
+       }
