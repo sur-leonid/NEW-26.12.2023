db53136b62c524926d20b52cc924144a6c7746f1
Surikov Leonid <Sur-leonid@yandex.ru>
2023-02-08 23:04:51 +0300

    add 048

--- 048/Program.cs
+++ 048/Program.cs
@@ -2,35 +2,28 @@
 // Создайте массив, который является произведением пар чисел в одномерном массиве 
 //a. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
-
-int N=0;
+int N = 0;
 System.Console.Write("Введите Длинну массива:");
-N=Convert.ToInt32(Console.ReadLine());
+N = Convert.ToInt32(Console.ReadLine());
 int[] a=new int[N];
 Random random=new Random();
-if (N%2!=0)
-  System.Console.WriteLine($" {((N-1)/2)} индекс (считаем с 0- нулевого) в данном массиве не имеет пары:");      
-
-
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(0,11);  // задали  масссив N   из чисел  от 0 до 10  (11-1)  
 
 for(int i=0;i<a.Length;i++)
-    a[i]=random.Next(1,11);
+     System.Console.Write($" {a[i]} ");
+System.Console.WriteLine();   //просто наглядно показали- вывели массив на экран
 
-System.Console.Write($" массив {a.Length} чисел  : ");
-for(int i=0;i<a.Length;i++)
+
+ int sum = 0;                                    //***СУММА  ПРОИЗВЕДЕНИЙ  ПАР КРАЙНИХ ЗНАЧЕНИЙ***
+    for (int i = 0; i <a.Length / 2; i++) // тут условие( делим на2) т.е. Начальный элемент движется от начала до середины, а в это же время  Карйний с конца элемент    
+     {                                     //  движется от КОнца до середины, т.е  они встретятся и дальше середины нет смысла идти, поэтому и Массив  / 2  
+       sum =sum + a[i] * a[a.Length - 1 - i];         //  сумма =  сумма + (начальный i * на последний i)    и перескакиваем по счетчику  пока i не дойдет до середины 
+     }
  
-    System.Console.Write($" {a[i]} ");
-System.Console.WriteLine();
+    
+ for (int i = 0; i <a.Length / 2; i++)   
+ Console.WriteLine($"элемент {i} * элемент {a.Length - i - 1},  {a[i]}*{a[a.Length - i - 1]} = {a[i] * a[a.Length - i - 1]}");  
+  //                 элемент {0} * элемент {1}               ,  {2}   *  {3}                 =   {4}       
 
-System.Console.Write("массив  чисел  с 10 по 99 : ");
-for(int i=10;i<99;i++)
-    System.Console.Write($"{a[i]} ");
-System.Console.WriteLine();
-
-
-int num = 0;
-for(int i=10;i<99;i++)
-if (i>=10 && i<=99)
-num++;
-  
-System.Console.Write($" количество  чисел  с 10 по 99  = {num} :");
+ Console.WriteLine($"сумма произведений пар массива =  {sum}");
