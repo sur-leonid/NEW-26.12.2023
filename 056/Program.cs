0747c3b5317c30b39da30d5273f14eb0e2e63c13
Surikov Leonid <Sur-leonid@yandex.ru>
2023-02-12 16:17:29 +0300

    add 056

---
+++ 056/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 56. Написать программу копирования массива
+
+int[] a= {1, 2, 3, 4, 5};
+//int[] b=new int[a.Length];
+//string s;
+//Array.Copy(a,b,a.Length);
+
+PrintArray(a);
+CopyArray(a);
+System.Console.WriteLine();
+PrintArray(a);
+
+void PrintArray(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i]} ");
+}
+void Swap(ref int a, ref int b)
+{
+    int t=a;
+    a=b;
+    b=t;
+}
+
+void CopyArray(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+    {
+        Swap(ref a[i],ref a[i]);
+    }
+
+}
