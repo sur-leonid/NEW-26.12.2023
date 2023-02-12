4bbecf04f87278e924a0191c4b17bda10d0e29cf
Surikov Leonid <Sur-leonid@yandex.ru>
2023-02-12 16:04:05 +0300

    add 054

---
+++ 054/Program.cs
@@ -0,0 +1,17 @@
+﻿// 54 заждача. С клавиатуры вводится число N. 
+//Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+Double  Fibonacci(int n) // подпрограмма   фибоначи //здесь можно  чтобы возращался  формат или  "- int "  или побольше   "- Double"
+{
+ if(n == 0 || n == 1) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+int N=0;
+System.Console.WriteLine("Введите число :");
+N=Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < N; i++)   // задаем  поиск на перве  40 чисел в ряде фибоначи
+{
+ //Console.WriteLine(Fibonacci(i));
+  Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
