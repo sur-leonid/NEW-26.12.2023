f2f754cd8975ed78ccc23c11b3cf91eff0d5b76e
Surikov Leonid <Sur-leonid@yandex.ru>
2023-02-19 18:58:55 +0300

    add 078

---
+++ 078/Program.cs
@@ -0,0 +1,20 @@
+﻿// задача 78. С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1. Использовать рекурсию.
+
+
+int Fib(int N)
+{
+    if (N==0) return 0;
+        else
+            if (N==1) return 1;
+                else
+                    return Fib(N-1)+Fib(N-2);
+}
+
+    System.Console.WriteLine(Fib(0));
+    System.Console.WriteLine(Fib(1));
+    System.Console.WriteLine($"{Fib(2)} сумма двухпредыдущих чисел ");
+    System.Console.WriteLine($"{Fib(3)} сумма двухпредыдущих чисел ");
+    System.Console.WriteLine($"{Fib(4)} сумма двухпредыдущих чисел ");
+    System.Console.WriteLine($"{Fib(5)} сумма двухпредыдущих чисел ");
+    System.Console.WriteLine($"{Fib(6)} сумма двухпредыдущих чисел ");
+    System.Console.WriteLine($"{Fib(7)} сумма двухпредыдущих чисел ");
