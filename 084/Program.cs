8c125bf9b532dfc3bee42adc17a2237aebe103a8
Surikov Leonid <Sur-leonid@yandex.ru>
2023-02-22 19:48:30 +0300

    add 084

---
+++ 084/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//84.Определить являются ли введенные с клавиатуры символы правильно записью целого числа. Вычислить сумму цифр введенного числа     
+
+            string num = Console.ReadLine();
+            int temp = 0;
+            if (!Int32.TryParse(num, out temp))
+                Console.WriteLine("Это не является верной записью целого");
+                else 
+                {
+                int sum = 0;
+                foreach (char digit in num)
+                    sum += digit - 48;
+                System.Console.WriteLine($"сумма чисел = {sum}:");
+                }
+        
+ 
