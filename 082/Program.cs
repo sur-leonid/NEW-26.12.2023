1dd2472c1a300a6d81c62ab0c524afce018adb3e
Surikov Leonid <Sur-leonid@yandex.ru>
2023-02-24 13:55:40 +0300

    add 088

--- 082/Program.cs
+++ 082/Program.cs
@@ -1,3 +1,15 @@
 ﻿// Задача 82. C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
 
+string s="1232123.1232";
 
+ int i=0;
+ while (i<s.Length && s[i]!='.') i++;
+    if (i==s.Length) 
+     System.Console.WriteLine(-1);
+    else
+     System.Console.WriteLine(i);
+
+
+System.Console.WriteLine(s.Split('.')[0].Length);
+
+//System.Console.WriteLine(s.IndexOf('.'));  // ЭТО СПОСОБ короче гораааздо 
---
+++ 088/Program.cs
@@ -0,0 +1,9 @@
+﻿// задача 88. Заменить все вхождения подстроки w в строке st на строку v. Вывести на экран первоначальную строку и конечную строку
+string s="wwoooww";
+System.Console.WriteLine(s);
+System.Console.WriteLine();
+string s1="w";
+string s2="v";
+
+s=s.Replace(s1,s2);
+System.Console.WriteLine(s);
